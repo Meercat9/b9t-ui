aefd2c4da92bf003bbe67f394bf05d7b1a33c74d
Billy Moffat <billymoffat92@hotmail.com>
2024-09-18 06:05:46 +0000

    Add dev container, autoformatting, tidy demo, fix tailwind

--- demo/main.tsx
+++ demo/main.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import { Button } from '../dist'
+import '../src/styles/globals.css'
+
+const App = () => (
+	<div>
+		<Button onClick={() => alert('Clicked!')}>Click Me</Button>
+	</div>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'))
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from 'react'
 
 type ButtonProps = {
 	onClick: () => void
 	children: React.ReactNode
-};
+}
 
 const Button: React.FC<ButtonProps> = ({ onClick, children }) => (
 	<button onClick={onClick} className="px-4 py-2 bg-blue-600 text-white">
-	{children}
+		{children}
 	</button>
 )
 
